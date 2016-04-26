2cfb41dea15cc9674ac3c846efdfbead6c99573e
Guido Kerkhove <guido_rulz@hotmail.com>
2016-04-26 13:44:04 +0200

    Made start screen

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class StartMenu : MonoBehaviour {
+
+    public Button First;
+    public GameObject Panel1;
+    public GameObject Panel2;
+	// Use this for initialization
+	void Start () {
+        First.onClick.AddListener(FirstClick);
+	}
+
+    void FirstClick()
+    {
+        print("lol");
+        Panel1.SetActive(false);
+        Panel2.SetActive(true);
+    }
+    
+}

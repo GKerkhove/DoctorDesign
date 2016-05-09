75defe608569962aa45cbede3079b4ee5e90dc58
Guido Kerkhove <guido_rulz@hotmail.com>
2016-05-09 10:59:59 +0200

    Start Screen first draft

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,11 +7,17 @@ using UnityEngine.EventSystems;
 public class StartMenu : MonoBehaviour {
 
     public Button First;
+    public Button Second;
+    public Button Third;
     public GameObject Panel1;
     public GameObject Panel2;
+    public GameObject Panel3;
+    public GameObject Panel4;
 	// Use this for initialization
 	void Start () {
         First.onClick.AddListener(FirstClick);
+        Second.onClick.AddListener(SecondClick);
+        Third.onClick.AddListener(ThirdClick);
 	}
 
     void FirstClick()
@@ -19,6 +25,18 @@ public class StartMenu : MonoBehaviour {
         print("lol");
         Panel1.SetActive(false);
         Panel2.SetActive(true);
+    }
+    void SecondClick()
+    {
+        print("lol");
+        Panel2.SetActive(false);
+        Panel3.SetActive(true);
+    }
+    void ThirdClick()
+    {
+        print("lol");
+        Panel3.SetActive(false);
+        Panel4.SetActive(true);
     }
     
 }

0cf999d495e7cbe08264eff470202770e8923d1d
Jimi <jimi.verhoeven@gmail.com>
2016-05-20 15:17:14 +0200

    Updated the profilepicture thing, still needs to send to server

--- Assets/Scripts/TakeProfilePicture.cs
+++ Assets/Scripts/TakeProfilePicture.cs
@@ -4,11 +4,9 @@ using UnityEngine.UI;
 
 public class TakeProfilePicture : MonoBehaviour {
     WebCamTexture _CamTex;
-    private string _SavePath = "C:/WebcamSnaps/";
-    int _CaptureCounter = 0;
     public Button startPhoto;
     public Button takePhoto;
-
+    public Image profilePicture;
     // Use this for initialization
     void Start()
     {
@@ -20,16 +18,27 @@ public class TakeProfilePicture : MonoBehaviour {
 
     void TakeSnapshot()
     {
+        _CamTex = Game.Get().qrController.e_DeviceController.cameraTexture;
         Texture2D snap = new Texture2D(_CamTex.width, _CamTex.height);
         snap.SetPixels(_CamTex.GetPixels());
         snap.Apply();
+        //WWWForm form = new WWWForm();
+        //form.AddField("frameCount", Time.frameCount.ToString());
+        //form.AddBinaryData("fileUpload", snap.GetRawTextureData(), "screenShot.png", "image/png");
+        //WWW w = new WWW("http://jimiverhoeven.nl:8080/uploadImage", form);
+        //yield return w;
 
-        System.IO.File.WriteAllBytes(_SavePath + _CaptureCounter.ToString() + ".png", snap.EncodeToPNG());
-        ++_CaptureCounter;
+        takePhoto.gameObject.SetActive(false);
+        Game.Get().qrController.StopCamera();
+        profilePicture.gameObject.SetActive(true);
+        profilePicture.sprite = Sprite.Create(snap, new Rect(0, 0, snap.width, snap.height), new Vector2(0.5f, 0.5f));
+
+
     }
 
     void FirstClick()
     {
         Game.Get().qrController.StartCamera();
+        takePhoto.gameObject.SetActive(true);
     }
 }

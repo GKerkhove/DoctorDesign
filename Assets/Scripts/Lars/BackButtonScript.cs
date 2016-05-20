d59ce00cfd15161669dfc0156cc0a9967a7f76c9
ElPinda <erep3lars@hotmail.com>
2016-05-20 11:09:49 +0200

    icon

---
+++ Assets/Scripts/Lars/BackButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButtonScript : MonoBehaviour {
+
+    public GameObject thisPage;
+    public GameObject lastPage;
+
+    public void Back()
+    {
+        thisPage.active = false;
+        lastPage.active = true;
+    }
+}

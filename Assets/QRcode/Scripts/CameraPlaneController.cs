1cd1a6f106938fa8a4defddad8dd19ab6395e030
Guido Kerkhove <guido_rulz@hotmail.com>
2016-06-23 14:06:05 +0200

    Rotation thingy

--- Assets/QRcode/Scripts/CameraPlaneController.cs
+++ Assets/QRcode/Scripts/CameraPlaneController.cs
@@ -21,7 +21,50 @@ public class CameraPlaneController : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Awake () {
-		
+		float Screenheight = (float)_targetCam.orthographicSize* 2.0f; 
+ 		float Screenwidth = Screenheight * Screen.width / Screen.height;
+ 		height = Screenheight ;
+ 		width = Screenwidth;
+ 		this.transform.localPosition = new Vector3(0,0,91.6f);
+ 
+ 		#if UNITY_EDITOR
+ 		transform.localEulerAngles = new Vector3(90,180,0);
+ 		transform.localScale = new Vector3(width/10, 1.0f, height/10);
+ 		#elif UNITY_WEBPLAYER
+ 		transform.localEulerAngles = new Vector3(90,180,0);
+ 		transform.localScale = new Vector3(width/10, 1.0f, height/10);
+ 		#endif
+ 
+ 		orientation = Screen.orientation;
+ 
+ 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+ 		if (Screen.orientation == ScreenOrientation.Portrait||
+ 		    Screen.orientation == ScreenOrientation.PortraitUpsideDown) {
+ 
+ 			#if UNITY_ANDROID
+ 			transform.localEulerAngles = new Vector3(0,270,90);
+ 			transform.localScale = new Vector3(height/10, 1.0f, width/10);
+ 			#elif UNITY_IOS
+ 			if( Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+ 			{
+ 				transform.localEulerAngles = new Vector3(0,270,90);
+ 			}
+ 			else
+ 		{
+ 				transform.localEulerAngles = new Vector3(0,90,270);
+ 			}
+ 			transform.localScale = new Vector3(-1*height/10, 1.0f, width/10);
+ 			#endif
+ 		} else if (Screen.orientation == ScreenOrientation.Landscape) {
+ 			#if UNITY_ANDROID
+ 			transform.localEulerAngles = new Vector3(90,180,0);
+ 			transform.localScale = new Vector3(width/10, 1.0f, height/10);
+ 			#elif UNITY_IOS
+ 			transform.localEulerAngles = new Vector3(-90,0,0);
+ 			transform.localScale = new Vector3(-1*width/10, 1.0f, height/10);
+ 			
+ 			#endif
+ 		}
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 	}
 
